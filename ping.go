598326fd8dd82f43cf7095dd1207b293262e1f89
Mannix <jun265731483@gmail.com>
2023-09-22 18:26:15 +0800

    init repo

---
+++ ping.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"os/signal"
+	"ping"
+	"time"
+)
+
+func main() {
+	srcIP := flag.String("S", "", "source ip")
+	flag.Parse()
+
+	host := flag.Arg(0)
+
+	logChan := make(chan string)
+
+	endChan := make(chan os.Signal, 1)
+
+	signal.Notify(endChan, os.Interrupt)
+
+	go func() {
+		for s := range logChan {
+			print(s)
+		}
+	}()
+
+	ping, _ := ping.New(*srcIP, host, logChan)
+
+	go ping.Run()
+
+	<-endChan
+	ping.Stop()
+	time.Sleep(time.Millisecond * 100)
+}
